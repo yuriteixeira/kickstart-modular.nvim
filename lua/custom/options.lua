7120f5ed1810c8145cae134c30659c13b80eec43
Yuri Teixeira <oyuriteixeira@gmail.com>
2026-03-17 11:35:57 +0100

    Nvim: Pure-text terminal support

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,6 +1,5 @@
 local graphical = os.getenv 'TERM' ~= 'linux'
 
-if graphical then
-  vim.o.termguicolors = true
-  vim.g.have_nerd_font = true
-end
+if graphical then vim.g.have_nerd_font = true end
+
+vim.o.termguicolors = true
--- lua/custom/overrides.lua
+++ lua/custom/overrides.lua
@@ -1,6 +1,10 @@
 local graphical = os.getenv 'TERM' ~= 'linux'
 
 if graphical then
+  -- TODO: Convert to Neovim's Lua API calls (like we see below)
   vim.cmd [[ highlight Normal guibg=none ctermbg=none ]]
   vim.cmd [[ highlight NonText guibg=none ctermbg=none ]]
+else
+  vim.api.nvim_set_hl(0, 'Visual', { bg = 'gray', fg = 'white', bold = true })
+  vim.api.nvim_set_hl(0, 'VisualNOS', { bg = 'gray', fg = 'white', bold = true })
 end
