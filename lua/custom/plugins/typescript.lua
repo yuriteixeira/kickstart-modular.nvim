5869f1a84acceabcdd16c89bb754493ea463d545
Yuri Teixeira <oyuriteixeira@gmail.com>
2026-03-17 12:18:05 +0100

    feat(typescript): support added

---
+++ lua/custom/plugins/typescript.lua
@@ -0,0 +1,9 @@
+-- typescript-tools
+---@module 'lazy'
+---@type LazySpec
+return {
+  'pmizio/typescript-tools.nvim',
+  lazy = false,
+  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  opts = {},
+}
