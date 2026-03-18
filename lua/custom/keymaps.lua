126ed4f1852c64d3f0c4630c159eef886682bb53
Yuri Teixeira <oyuriteixeira@gmail.com>
2026-03-18 14:34:28 +0100

    feat(custom): add zoom helper and keymap

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -3,3 +3,7 @@ local map = vim.keymap.set
 map('n', '<leader>ww', ':w<CR>', { desc = 'File: Save' })
 map('n', '<leader><leader>', ':b#<CR>', { desc = 'Buffers: Toggle current buffer with last opened one' })
 map('n', '<leader>to', '<cmd>Outline<CR>', { desc = 'Toggle: Outline' })
+
+-- [[ Zoom functionality ]]
+local zoom = require 'custom.helpers.zoom'
+map('n', '<leader>z', zoom.toggle, { desc = 'Toggle: Zoom current buffer' })
