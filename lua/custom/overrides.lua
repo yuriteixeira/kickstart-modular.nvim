cadb01cdce942f5120e667b9a490f5603d7289de
Yuri Teixeira <oyuriteixeira@gmail.com>
2026-03-19 12:33:56 +0100

    chore(overrides): better eval and comments

--- lua/custom/overrides.lua
+++ lua/custom/overrides.lua
@@ -1,10 +1,13 @@
-local graphical = os.getenv 'TERM' ~= 'linux'
+-- Highlight's tweaking
+local graphical = os.getenv 'TERM' ~= 'linux' or os.getenv 'TERM' ~= 'console'
 
 if graphical then
+  -- Transparent Bg
   -- TODO: Convert to Neovim's Lua API calls (like we see below)
   vim.cmd [[ highlight Normal guibg=none ctermbg=none ]]
   vim.cmd [[ highlight NonText guibg=none ctermbg=none ]]
 else
+  -- Non-graphical highlight override (since it has only 8 colors, beyond base16)
   vim.api.nvim_set_hl(0, 'Visual', { bg = 'gray', fg = 'white', bold = true })
   vim.api.nvim_set_hl(0, 'VisualNOS', { bg = 'gray', fg = 'white', bold = true })
 end
