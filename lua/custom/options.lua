c5b27f6feaa9dc43ca523a3ef3ff76b5e9666836
Yuri Teixeira <oyuriteixeira@gmail.com>
2026-03-24 16:27:45 +0100

    feat: editorconfig-like defaults

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -3,3 +3,10 @@ local graphical = os.getenv 'TERM' ~= 'linux' or os.getenv 'TERM' ~= 'console'
 if graphical then vim.g.have_nerd_font = true end
 
 vim.o.termguicolors = true
+
+-- Sane defaults (overridden by .editorconfig when present)
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.fileencoding = 'utf-8'
