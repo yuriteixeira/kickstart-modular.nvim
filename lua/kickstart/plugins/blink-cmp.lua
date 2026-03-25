4ce7d977a70096fb6df62c0bfbeb412972be3bfe
Yuri Teixeira <oyuriteixeira@gmail.com>
2026-03-25 10:12:46 +0100

    feat(blink.cmp): tab also accepts selection

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -58,6 +58,8 @@ return {
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'enter',
 
+        ['<Tab>'] = { 'select_and_accept', 'snippet_forward', 'fallback' },
+
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
