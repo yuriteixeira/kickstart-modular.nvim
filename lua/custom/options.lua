3e5363dce50a5d551d4f1f7e869bfaefd20dee67
Yuri Teixeira <oyuriteixeira@gmail.com>
2026-04-17 11:28:23 +0200

    feat: rounded borders for floating windows

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,7 +1,9 @@
+-- Icons and such will show only when a graphical env is up 
 local graphical = os.getenv 'TERM' ~= 'linux' or os.getenv 'TERM' ~= 'console'
 
 if graphical then vim.g.have_nerd_font = true end
 
+-- Needed to make it play well with base16 shell themes even in "no gui" console
 vim.o.termguicolors = true
 
 -- Sane defaults (overridden by .editorconfig when present)
@@ -10,3 +12,6 @@ vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.fileencoding = 'utf-8'
+
+-- Use rounded borders for floating windows such as LSP hover, diagnostics, and popup docs.
+vim.o.winborder = 'rounded'
