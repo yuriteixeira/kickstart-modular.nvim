9636f9a47ce717c82e9de630dab10f4b323ce280
Yuri Teixeira <oyuriteixeira@gmail.com>
2026-08-14 14:49:21 +0200

    feat(plugin): diffconflicts for git mergetool
    Simpler git conflict resolutions with 2-way view by default +
    Full 3-way history in another tab.
    
    More: https://github.com/whiteinge/diffconflicts

---
+++ lua/custom/plugins/diffconflicts.lua
@@ -0,0 +1,12 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  'whiteinge/diffconflicts',
+  cmd = {
+    'DiffConflicts',
+    'DiffConflictsShowHistory',
+    'DiffConflictsWithHistory',
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
