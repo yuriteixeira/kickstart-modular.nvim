72070921ab9d9ac4ca4ea5a8305ab73328dbed51
Yuri Teixeira <oyuriteixeira@gmail.com>
2026-08-25 16:17:27 +0200

    feat: zen mode

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -12,9 +12,10 @@ map('n', '<leader>td', diagnostics.toggle_inline, { desc = 'Toggle: Inline diagn
 local indent_info = require 'custom.helpers.indent-info'
 vim.api.nvim_create_user_command('IndentInfo', indent_info.show, { desc = 'Show indentation settings and their origin' })
 
--- [[ Zoom functionality ]]
+-- [[ Zoom and focus modes ]]
 local zoom = require 'custom.helpers.zoom'
 map('n', '<leader>z', zoom.toggle, { desc = 'Toggle: Zoom current buffer' })
+map('n', '<leader>zz', function() require('zen-mode').toggle() end, { desc = 'Toggle: Zen mode' })
 
 -- [[ Markdown preview ]]
 local markdown = require 'custom.helpers.glow-markdown'
