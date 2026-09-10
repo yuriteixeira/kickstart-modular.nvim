88502c8be0efebafb7e7bc0f6ead2504688d3ec9
Yuri Teixeira <oyuriteixeira@gmail.com>
2026-09-10 19:26:27 +0200

    refactor(commands): centralize user command registrations
    Keep command definitions in the dedicated commands module so the keymaps module only configures keybindings. This preserves the existing command names and behavior.

--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -1,5 +1,23 @@
+local cmd = vim.api.nvim_create_user_command
+local file_path = require 'custom.helpers.file-path'
+local indent_info = require 'custom.helpers.indent-info'
+local qq = require 'custom.helpers.qq'
+
 local function update_plugins() vim.pack.update() end
 
-vim.api.nvim_create_user_command('PackUpdate', update_plugins, {
+cmd('PackUpdate', update_plugins, {
   desc = 'Update plugins managed by vim.pack',
+})
+
+cmd('FilePath', file_path.copy_relative, {
+  desc = 'Copy relative file path to clipboard',
+})
+
+cmd('IndentInfo', indent_info.show, {
+  desc = 'Show indentation settings and their origin',
+})
+
+cmd('QQ', qq.run_stream, {
+  nargs = '+',
+  desc = 'Stream a quick answer to a question into the current buffer',
 })
--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,5 +1,4 @@
 local map = vim.keymap.set
-local cmd = vim.api.nvim_create_user_command
 
 map(
   'n',
@@ -16,17 +15,11 @@ map('n', '<leader>ww', ':w<CR>', { desc = 'File: Save' })
 map('n', '<leader>to', ':Outline<CR>', { desc = 'Toggle: Outline' })
 
 -- [[ File path ]]
-local file_path = require 'custom.helpers.file-path'
-cmd('FilePath', file_path.copy_relative, { desc = 'Copy relative file path to clipboard' })
 map('n', '<leader>fp', ':FilePath<CR>', { desc = 'File: Copy relative file path to clipboard' })
 
 -- [[ Diagnostics ]]
 local diagnostics = require 'custom.helpers.diagnostics'
 map('n', '<leader>td', diagnostics.toggle_inline, { desc = 'Toggle: Inline diagnostics' })
-
--- [[ Indentation info ]]
-local indent_info = require 'custom.helpers.indent-info'
-cmd('IndentInfo', indent_info.show, { desc = 'Show indentation settings and their origin' })
 
 -- [[ Zoom ]]
 local zoom = require 'custom.helpers.zoom'
@@ -40,6 +33,4 @@ local markdown = require 'custom.helpers.glow-markdown'
 map('n', '<leader>tm', markdown.preview, { desc = 'Toggle: Markdown preview' })
 
 -- [[ QQ command: AI answers streamed to the nvim buffer ]]
-local qq = require 'custom.helpers.qq'
-cmd('QQ', qq.run_stream, { nargs = '+', desc = 'Stream a quick answer to a question into the current buffer' })
 map('n', '<leader>qq', ':QQ ', { desc = 'AI: Quick Question' })
