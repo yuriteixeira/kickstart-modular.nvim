937cf9eb1ead043dc2e5ab5b53fc785eb97e3fbc
Yuri Teixeira <oyuriteixeira@gmail.com>
2026-09-22 14:24:42 +0200

    fix(lsp): rename operation now propagates

--- lua/custom/plugins/autosave.lua
+++ lua/custom/plugins/autosave.lua
@@ -1,4 +1,8 @@
 local function gh(repo) return 'https://github.com/' .. repo end
 
 vim.pack.add { { src = gh 'okuuva/auto-save.nvim', version = vim.version.range '1.*' } }
-require('auto-save').setup {}
+require('auto-save').setup {
+  -- LSP operations can edit files in hidden buffers, such as TypeScript rename.
+  -- Save every modified buffer so those workspace edits reach disk.
+  write_all_buffers = true,
+}
