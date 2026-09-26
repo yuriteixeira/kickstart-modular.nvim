d08ecc238de5ef3a1aef465c20129bcaafdf7f82
Yuri Teixeira <oyuriteixeira@gmail.com>
2026-09-26 17:54:24 +0200

    feat(review-comments): more shortcuts

--- lua/custom/plugins/review-comments/init.lua
+++ lua/custom/plugins/review-comments/init.lua
@@ -29,19 +29,26 @@ command('ReviewCommentExport', function(opts) review.export(opts.args) end, {
 vim.keymap.set('n', '<leader>rc', '<cmd>ReviewCommentAdd<cr>', { desc = 'Review: Comment on line' })
 vim.keymap.set('x', '<leader>rc', ":<C-u>'<,'>ReviewCommentAdd<cr>", { desc = 'Review: Comment on range' })
 vim.keymap.set('n', '<leader>rC', '<cmd>ReviewCommentAdd file<cr>', { desc = 'Review: Comment on file' })
+vim.keymap.set('n', '<leader>re', '<cmd>ReviewCommentEdit<cr>', { desc = 'Review: Edit comment' })
+vim.keymap.set('n', '<leader>rd', '<cmd>ReviewCommentDelete<cr>', { desc = 'Review: Delete comment' })
 vim.keymap.set('n', '<leader>rl', '<cmd>ReviewCommentList<cr>', { desc = 'Review: List comments' })
 vim.keymap.set('n', '<leader>ro', '<cmd>ReviewCommentOverview<cr>', { desc = 'Review: Files with comments' })
 vim.keymap.set('n', ']r', '<cmd>ReviewCommentNext<cr>', { desc = 'Review: Next comment' })
 vim.keymap.set('n', '[r', '<cmd>ReviewCommentPrevious<cr>', { desc = 'Review: Previous comment' })
 vim.keymap.set('n', '<leader>rt', '<cmd>ReviewCommentToggle<cr>', { desc = 'Review: Toggle inline comments' })
+vim.keymap.set('n', '<leader>ry', '<cmd>ReviewCommentCopy<cr>', { desc = 'Review: Copy as Markdown' })
+vim.keymap.set('n', '<leader>rx', '<cmd>ReviewCommentExport<cr>', { desc = 'Review: Export as Markdown file' })
 
-local group = vim.api.nvim_create_augroup('CustomReviewComments', { clear = true })
+local group = vim.api.nvim_create_augroup('ReviewComments', { clear = true })
+
 vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile', 'BufEnter' }, {
   group = group,
   callback = function(args) review.attach(args.buf) end,
 })
+
 vim.api.nvim_create_autocmd({ 'TextChanged', 'TextChangedI', 'BufLeave', 'VimLeavePre' }, {
   group = group,
   callback = function() review.flush() end,
 })
+
 review.attach(vim.api.nvim_get_current_buf())
